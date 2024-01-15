7476f3fbb6bdf5cb3786e1e0ac8032e31137a56b
Yar Night <yaroslav239@gmail.com>
2024-01-15 20:10:45 +0300

    feat: add task14

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -546,8 +546,21 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+  for (let i = 1; i <= iterations; i += 1) {
+    let a = '';
+    let b = '';
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2 === 0) a += result[j];
+      else {
+        b += result[j];
+      }
+    }
+    result = a + b;
+    if (result === str) return shuffleChar(str, iterations % i);
+  }
+  return result;
 }
 
 /**
